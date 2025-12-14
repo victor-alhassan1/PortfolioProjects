e12bef6111f38dda7aeb8719f3bf166d2e9a51e3
victor-alhassan1 <alhassanvictor89@gmail.com>
2025-12-14 23:56:38 +0100

    Add files via upload

---
+++ COVID portfolio project.sql
@@ -0,0 +1,407 @@
+-- DROP TABLE covid_deaths;
+
+-- CREATE TABLE covid_deaths (
+-- 	iso_code VARCHAR(10),
+--     continent VARCHAR(20),
+-- 	location VARCHAR(100),
+-- 	date DATE,
+-- 	total_cases DOUBLE,
+--     new_cases DOUBLE,
+--     new_cases_smoothed DOUBLE,
+--     total_deaths DOUBLE,
+--     new_deaths DOUBLE,
+--     new_deaths_smoothed DOUBLE,
+-- 	total_cases_per_million DOUBLE,
+-- 	new_cases_per_million DOUBLE,
+-- 	new_cases_smoothed_per_million DOUBLE,
+-- 	total_deaths_per_million DOUBLE,
+-- 	new_deaths_per_million DOUBLE,
+-- 	new_deaths_smoothed_per_million DOUBLE,
+-- 	reproduction_rate DOUBLE,
+-- 	icu_patients DOUBLE,
+-- 	icu_patients_per_million DOUBLE,
+-- 	hosp_patients DOUBLE,
+-- 	hosp_patients_per_million DOUBLE,
+-- 	weekly_icu_admissions DOUBLE,
+-- 	weekly_icu_admissions_per_million DOUBLE,
+-- 	weekly_hosp_admissions DOUBLE,
+-- 	weekly_hosp_admissions_per_million DOUBLE,
+-- 	new_tests DOUBLE,
+-- 	total_tests DOUBLE,
+-- 	total_tests_per_thousand DOUBLE,
+-- 	new_tests_per_thousand DOUBLE,
+-- 	new_tests_smoothed DOUBLE,
+-- 	new_tests_smoothed_per_thousand DOUBLE,
+-- 	positive_rate DOUBLE,
+-- 	tests_per_case DOUBLE,
+-- 	tests_units DOUBLE,
+-- 	total_vaccinations DOUBLE,
+-- 	people_vaccinated DOUBLE,
+-- 	people_fully_vaccinated DOUBLE,
+-- 	new_vaccinations DOUBLE,
+-- 	new_vaccinations_smoothed DOUBLE,
+-- 	total_vaccinations_per_hundred DOUBLE,
+-- 	people_vaccinated_per_hundred DOUBLE,
+-- 	people_fully_vaccinated_per_hundred DOUBLE,
+-- 	new_vaccinations_smoothed_per_million DOUBLE,
+-- 	stringency_index DOUBLE,
+-- 	population DOUBLE,
+-- 	population_density DOUBLE,
+-- 	median_age DOUBLE,
+-- 	aged_65_older DOUBLE,
+-- 	aged_70_older DOUBLE,
+-- 	gdp_per_capita DOUBLE,
+-- 	extreme_poverty DOUBLE,
+-- 	cardiovasc_death_rate DOUBLE,
+--     diabetes_prevalence DOUBLE,
+-- 	female_smokers DOUBLE,
+-- 	male_smokers DOUBLE,
+-- 	handwashing_facilities DOUBLE,
+-- 	hospital_beds_per_thousand DOUBLE,
+-- 	life_expectancy DOUBLE,
+-- 	human_development_index DOUBLE
+-- ) CHARACTER SET utf8mb4;
+
+-- LOAD DATA LOCAL INFILE 'C:/Users/Victor/Documents/CovidDeaths.csv'
+-- INTO TABLE covid_deaths
+-- FIELDS TERMINATED BY ','
+-- ENCLOSED BY '"'
+-- LINES TERMINATED BY '\n'
+-- IGNORE 1 ROWS
+-- (
+-- 	iso_code,
+--     continent,
+-- 	location,
+-- 	date,
+-- 	total_cases,
+--     new_cases,
+--     new_cases_smoothed,
+--     total_deaths,
+--     new_deaths,
+--     new_deaths_smoothed,
+-- 	total_cases_per_million,
+-- 	new_cases_per_million,
+-- 	new_cases_smoothed_per_million,
+-- 	total_deaths_per_million,
+-- 	new_deaths_per_million,
+-- 	new_deaths_smoothed_per_million,
+-- 	reproduction_rate,
+-- 	icu_patients,
+-- 	icu_patients_per_million,
+-- 	hosp_patients,
+-- 	hosp_patients_per_million,
+-- 	weekly_icu_admissions,
+-- 	weekly_icu_admissions_per_million,
+-- 	weekly_hosp_admissions,
+-- 	weekly_hosp_admissions_per_million,
+-- 	new_tests,
+-- 	total_tests,
+-- 	total_tests_per_thousand,
+-- 	new_tests_per_thousand,
+-- 	new_tests_smoothed,
+-- 	new_tests_smoothed_per_thousand,
+-- 	positive_rate,
+-- 	tests_per_case,
+-- 	tests_units,
+-- 	total_vaccinations,
+-- 	people_vaccinated,
+-- 	people_fully_vaccinated,
+-- 	new_vaccinations,
+-- 	new_vaccinations_smoothed,
+-- 	total_vaccinations_per_hundred,
+-- 	people_vaccinated_per_hundred,
+-- 	people_fully_vaccinated_per_hundred,
+-- 	new_vaccinations_smoothed_per_million,
+-- 	stringency_index,
+-- 	population,
+-- 	population_density,
+-- 	median_age,
+-- 	aged_65_older,
+-- 	aged_70_older,
+-- 	gdp_per_capita,
+-- 	extreme_poverty,
+-- 	cardiovasc_death_rate,
+--     diabetes_prevalence,
+-- 	female_smokers,
+-- 	male_smokers,
+-- 	handwashing_facilities,
+-- 	hospital_beds_per_thousand,
+-- 	life_expectancy,
+-- 	human_development_index 
+-- )
+-- SET
+-- 	iso_code = NULLIF(iso_code, ''),
+-- 	continent = NULLIF(continent, ''),
+-- 	location = NULLIF(location, ''),
+-- 	total_cases = NULLIF(total_cases, ''),
+--     new_cases = NULLIF(new_cases, ''),
+--     new_cases_smoothed = NULLIF(new_cases_smoothed, ''),
+--     total_deaths = NULLIF(total_deaths, ''),
+--     new_deaths = NULLIF(new_deaths, ''),
+--     new_deaths_smoothed = NULLIF(new_deaths_smoothed, ''),
+-- 	total_cases_per_million = NULLIF(total_cases_per_million, ''),
+-- 	new_cases_per_million = NULLIF(new_cases_per_million, ''),
+-- 	new_cases_smoothed_per_million = NULLIF(new_cases_smoothed_per_million, ''),
+-- 	total_deaths_per_million = NULLIF(total_deaths_per_million, ''),
+-- 	new_deaths_per_million = NULLIF(new_deaths_per_million, ''),
+-- 	new_deaths_smoothed_per_million = NULLIF(new_deaths_smoothed_per_million, ''),
+-- 	reproduction_rate = NULLIF(reproduction_rate, ''),
+-- 	icu_patients = NULLIF(icu_patients, ''),
+-- 	icu_patients_per_million = NULLIF(icu_patients_per_million, ''),
+-- 	hosp_patients = NULLIF(hosp_patients, ''),
+-- 	hosp_patients_per_million = NULLIF(hosp_patients_per_million, ''),
+-- 	weekly_icu_admissions = NULLIF(weekly_icu_admissions, ''),
+-- 	weekly_icu_admissions_per_million = NULLIF(weekly_icu_admissions_per_million, ''),
+-- 	weekly_hosp_admissions = NULLIF(weekly_hosp_admissions, ''),
+-- 	weekly_hosp_admissions_per_million = NULLIF(weekly_hosp_admissions_per_million, ''),
+-- 	new_tests = NULLIF(new_tests, ''),
+-- 	total_tests = NULLIF(total_tests, ''),
+-- 	total_tests_per_thousand = NULLIF(total_tests_per_thousand, ''),
+-- 	new_tests_per_thousand = NULLIF(new_tests_per_thousand, ''),
+-- 	new_tests_smoothed = NULLIF(new_tests_smoothed, ''),
+-- 	new_tests_smoothed_per_thousand = NULLIF(new_tests_smoothed_per_thousand, ''),
+-- 	positive_rate = NULLIF(positive_rate, ''),
+-- 	tests_per_case = NULLIF(tests_per_case, ''),
+-- 	tests_units = NULLIF(tests_units, ''),
+-- 	total_vaccinations = NULLIF(total_vaccinations, ''),
+-- 	people_vaccinated = NULLIF(people_vaccinated, ''),
+-- 	people_fully_vaccinated = NULLIF(people_fully_vaccinated, ''),
+-- 	new_vaccinations = NULLIF(new_vaccinations, ''),
+-- 	new_vaccinations_smoothed = NULLIF(new_vaccinations_smoothed, ''),
+-- 	total_vaccinations_per_hundred = NULLIF(total_vaccinations_per_hundred, ''),
+-- 	people_vaccinated_per_hundred = NULLIF(people_vaccinated_per_hundred, ''),
+-- 	people_fully_vaccinated_per_hundred = NULLIF(people_vaccinated_per_hundred, ''),
+-- 	new_vaccinations_smoothed_per_million = NULLIF(new_vaccinations_smoothed_per_million, ''),
+-- 	stringency_index = NULLIF(stringency_index, ''),
+-- 	population_density = NULLIF(population_density, ''),
+-- 	median_age = NULLIF(median_age, ''),
+-- 	aged_65_older = NULLIF(aged_65_older, ''),
+-- 	aged_70_older = NULLIF(aged_70_older, ''),
+-- 	gdp_per_capita = NULLIF(gdp_per_capita, ''),
+-- 	extreme_poverty = NULLIF(extreme_poverty, ''),
+-- 	cardiovasc_death_rate = NULLIF(cardiovasc_death_rate, ''),
+-- 	diabetes_prevalence = NULLIF(diabetes_prevalence, ''),
+-- 	female_smokers = NULLIF(female_smokers, ''),
+-- 	male_smokers = NULLIF(male_smokers, ''),
+-- 	handwashing_facilities = NULLIF(handwashing_facilities, ''),
+-- 	hospital_beds_per_thousand = NULLIF(hospital_beds_per_thousand, ''),
+-- 	life_expectancy = NULLIF(life_expectancy, ''),
+-- 	human_development_index = NULLIF(human_development_index, ''); 
+
+-- SELECT *
+-- FROM covid_deaths
+-- -- LIMIT 50
+-- ;
+
+-- To Select the data that we're going to be using
+-- SELECT
+-- 	location,
+--     date,
+--     total_cases,
+--     new_cases,
+--     total_deaths,
+--     population
+-- FROM covid_deaths
+-- WHERE continent IS NOT NULL
+-- ORDER BY 1,2;
+
+-- Total Cases vs Total Deaths
+-- Shows the likelihood of dying if you contract COVID 
+-- SELECT
+-- 	location,
+--     date,
+--     total_cases,
+--     total_deaths,
+--     (total_deaths/total_cases)*100 AS deathpercentage
+-- FROM covid_deaths
+-- WHERE continent IS NOT NULL
+-- ORDER BY 1,2;
+
+-- TOTAL DEATHS VS TOTAL CASES IN NIGERIA
+-- SELECT
+-- 	location,
+--     date,
+--     total_cases,
+--     total_deaths,
+--     (total_deaths/total_cases)*100 AS deathpercentage
+-- FROM covid_deaths
+-- WHERE continent IS NOT NULL
+-- WHERE location like '%Nigeria%'
+-- ORDER BY 1,2 DESC;
+
+
+-- Shows the population that got COVID
+-- SELECT
+-- 	location,
+--     date,
+--     total_cases,
+--     population,
+--     (total_cases/population)* 100 AS deathpercentage
+--     FROM covid_deaths
+--     WHERE location like '%Nigeria%'
+--     ORDER BY 1,2;
+
+-- Looking at countries with the highest infection rate per population
+-- SELECT
+-- 	location,
+--     MAX(total_cases) AS highest_infection_count,
+--     population,
+--     MAX((total_cases/population))* 100 AS Percent_population_infected
+-- FROM covid_deaths
+-- WHERE continent IS NOT NULL
+ --    WHERE location like '%Nigeria%'
+-- GROUP BY location, population
+-- ORDER BY Percent_population_infected DESC;
+
+-- Showing countries witu the highest death count per population
+-- SELECT
+-- 	location,
+--     MAX(total_deaths) AS total_death_count
+-- FROM covid_deaths
+--  --    WHERE location like '%Nigeria%'
+-- WHERE continent IS NOT NULL
+-- GROUP BY location
+-- ORDER BY total_death_count DESC;
+
+-- -- BREAKING THIS DOWN BY CONTINENT
+-- SELECT
+-- 	continent,
+--     MAX(total_deaths) AS total_death_count
+-- FROM covid_deaths
+--  --    WHERE location like '%Nigeria%'
+-- WHERE continent IS NOT NULL
+-- GROUP BY continent
+-- ORDER BY total_death_count DESC;
+
+-- SELECT
+-- 	location,
+--     MAX(total_deaths) AS total_death_count
+-- FROM covid_deaths
+--  --    WHERE location like '%Nigeria%'
+-- WHERE continent IS NULL
+-- GROUP BY location
+-- ORDER BY total_death_count DESC;
+
+-- BREAKING THIS DOWN BY CONTINENT
+-- This shows the continents with the highest death counts
+
+-- SELECT
+-- 	continent,
+--     MAX(total_deaths) AS total_death_count
+-- FROM covid_deaths
+--  --    WHERE location like '%Nigeria%'
+-- WHERE continent IS NOT NULL
+-- GROUP BY continent
+-- ORDER BY total_death_count DESC;
+
+-- GLOBAL NUMBERS
+-- SELECT
+--     date,
+--     SUM(new_cases) AS total_cases,
+--     SUM(new_deaths) AS total_deaths,
+--     SUM(new_deaths)/SUM(new_cases)* 100 AS deathpercentage
+-- FROM covid_deaths
+-- WHERE continent IS NOT NULL
+-- -- WHERE location like '%Nigeria%'
+-- GROUP BY date
+-- ORDER BY 1,2;
+
+-- FOR THE TOTALS WITHOUT DATE
+-- SELECT
+    -- date,
+    -- SUM(new_cases) AS total_cases,
+--     SUM(new_deaths) AS total_deaths,
+--     SUM(new_deaths)/SUM(new_cases)* 100 AS deathpercentage
+-- FROM covid_deaths
+-- WHERE continent IS NOT NULL
+-- WHERE location like '%Nigeria%'
+-- GROUP BY date
+-- ORDER BY 1,2;
+
+
+-- Looking at Total Population vs vaccinations
+-- SELECT
+-- 	dea.continent,
+--     dea.location,
+--     dea.date,
+--     dea.population,
+--     vac.new_vaccinations,
+--     SUM(vac.new_vaccinations) OVER (PARTITION BY dea.location ORDER BY dea.location, dea.date) as rollingPeople_vaccinated
+-- FROM covid_deaths AS dea
+-- JOIN covid_vaccinations as vac
+-- ON dea.location = vac.location
+-- and dea.date = vac.date
+-- WHERE dea.continent IS NOT NULL
+-- ORDER BY 2,3;
+
+-- Using a CTE
+-- WITH PopvsVac (continent, location, date, population, new_vaccinations, rollingPeople_vaccinated)
+-- AS 
+-- (SELECT
+-- 	dea.continent,
+--     dea.location,
+--     dea.date,
+--     dea.population,
+--     vac.new_vaccinations,
+--     SUM(vac.new_vaccinations) OVER (PARTITION BY dea.location ORDER BY dea.location, dea.date) as rollingPeople_vaccinated
+-- FROM covid_deaths AS dea
+-- JOIN covid_vaccinations as vac
+-- ON dea.location = vac.location
+-- and dea.date = vac.date
+-- WHERE dea.continent IS NOT NULL
+-- -- ORDER BY 2,3
+-- )
+-- SELECT *, (rollingPeople_vaccinated/population)*100 AS Percentage_rolling_vaccinated
+-- FROM PopvsVac;
+
+-- FOR THE TEMP TABLE
+
+-- DROP TABLE IF EXISTS PercentPopulationVaccinated
+-- CREATE TABLE PercentPopulationVaccinated
+-- (
+-- Continent NVARCHAR(255),
+-- Location NVARCHAR(255),
+-- Date DATETIME,
+-- Population NUMERIC,
+-- New_vaccinations NUMERIC,
+-- RollingPeopleVaccinated NUMERIC
+-- );
+
+-- INSERT INTO PercentPopulationVaccinated
+-- SELECT
+-- 	dea.continent,
+--     dea.location,
+--     dea.date,
+--     dea.population,
+--     vac.new_vaccinations,
+--     SUM(vac.new_vaccinations) OVER (PARTITION BY dea.location ORDER BY dea.location, dea.date) as rollingPeople_vaccinated
+-- FROM covid_deaths AS dea
+-- JOIN covid_vaccinations as vac
+-- ON dea.location = vac.location
+-- and dea.date = vac.date
+-- WHERE dea.continent IS NOT NULL
+-- -- ORDER BY 2,3
+-- ;
+-- SELECT *, (RollingPeopleVaccinated/Population)*100 AS Percentage_rolling_vaccinated
+-- FROM PercentPopulationVaccinated;
+
+-- Creating View to store data for visualizations later
+-- CREATE VIEW Percent_Population_Vaccinated AS
+-- SELECT
+-- 	dea.continent,
+--     dea.location,
+--     dea.date,
+--     dea.population,
+--     vac.new_vaccinations,
+--     SUM(vac.new_vaccinations) OVER (PARTITION BY dea.location ORDER BY dea.location, dea.date) as rollingPeople_vaccinated
+-- FROM covid_deaths AS dea
+-- JOIN covid_vaccinations as vac
+-- ON dea.location = vac.location
+-- and dea.date = vac.date
+-- WHERE dea.continent IS NOT NULL
+-- -- ORDER BY 2,3
+-- ;
+
+-- SELECT 
+-- 	*
+-- FROM Percent_Population_Vaccinated;
